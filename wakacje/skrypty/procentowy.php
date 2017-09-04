7f22829903e8723b6f457ffe7399894ec881c239
Mateusz Murawski <matimaxi@o2.pl>
2017-09-04 16:08:49 +0200

    Poprawa skryptu liczacego procenty

--- wakacje/skrypty/procentowy.php
+++ wakacje/skrypty/procentowy.php
@@ -1,22 +1,33 @@
 <script>
-
-		var dzis = '<?= date('U') ?>';
 		
-		var wydarzenie = new Date(2018,5,22,12);
-		var wydarzeniex = wydarzenie.getTime();
+		var wydarzenie1 = new Date(2018,5,22,12);
+		var wydarzeniexx = new Date('<?= date('Y n j') ?>');
 		var wydarzenier = new Date(2017,8,4,12);
-		var x = wydarzenie - wydarzenier;
+		var x = wydarzenie1 - wydarzenier;
 
 function procentowy()
 	{
-		
-		var czas = wydarzeniex - wydarzenier;
-		var procent = parseInt((czas * 100)/x);
-		
-		 document.getElementById("procent").innerHTML = 
-		 procent + "% ";
+		if(wydarzeniexx>wydarzenie1)
+       {
+
+			document.getElementById("procent").innerHTML = 
+			"100% !!!";
 		 
 		 setTimeout("procentowy()",1000);
+
+        }
+		
+		else
+		{
+			
+			var czas = wydarzeniexx - wydarzenier;
+			var procent = parseInt((czas * 100)/x);
+				
+			document.getElementById("procent").innerHTML = 
+			procent + "% ";
+				 
+			setTimeout("procentowy()",1000);
+		}
 	
 	}
 	
